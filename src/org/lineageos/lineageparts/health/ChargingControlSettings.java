41b4557bc0f8c6e0e1b454ed2f49f6194d711181
Pranav Vashi <neobuddy89@gmail.com>
2023-06-19 00:02:45 +0530

    Use ListPreference for charging control fragment
    * DropDownPreference is 2013.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: ReveRTX <rev9rt@gmail.com>

--- src/org/lineageos/lineageparts/health/ChargingControlSettings.java
+++ src/org/lineageos/lineageparts/health/ChargingControlSettings.java
@@ -1,3 +1,4 @@
+
 /*
  * SPDX-FileCopyrightText: 2023 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
@@ -21,7 +22,7 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 
 import lineageos.health.HealthInterface;
-import lineageos.preference.LineageSystemSettingDropDownPreference;
+import lineageos.preference.LineageSystemSettingListPreference;
 import lineageos.preference.LineageSystemSettingMainSwitchPreference;
 import lineageos.providers.LineageSettings;
 
@@ -47,7 +48,7 @@ public class ChargingControlSettings extends SettingsPreferenceFragment implemen
     private static final String CHARGING_CONTROL_LIMIT_PREF = "charging_control_charging_limit";
 
     private LineageSystemSettingMainSwitchPreference mChargingControlEnabledPref;
-    private LineageSystemSettingDropDownPreference mChargingControlModePref;
+    private LineageSystemSettingListPreference mChargingControlModePref;
     private StartTimePreference mChargingControlStartTimePref;
     private TargetTimePreference mChargingControlTargetTimePref;
     private ChargingLimitPreference mChargingControlLimitPref;
